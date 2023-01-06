326b853a21364eb7ff72dda99d39ddf9004d4bb0
Hadia Shafique <hshafique@Hadias-MacBook-Air.local>
2023-01-06 13:19:52 -0600

    finsihed

---
+++ src/groceries.js
@@ -0,0 +1,20 @@
+export default [
+    {  item: 'Bread',
+    brand :'TexasToast',
+    units: '24 Oz',
+    quantity: 20,
+    isPurchased: false
+  },
+  {  item: 'Milk',
+  brand:'Fairlife',
+  units: '52 fl Oz',
+  quantity: 18,
+  isPurchased: false
+},
+{  item: 'Eggs',
+    brand :'GreatValue',
+    units: '60 Count',
+    quantity: 20,
+    isPurchased: false
+  }
+]
